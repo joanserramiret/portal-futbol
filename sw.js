406574882ef2d944df52b3e7f474e8877ff2205c
Joan Serra <joanserramiret@gmail.com>
2026-09-06 22:14:42 +0200

    resultados 06/09 22:14

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    asi se tira la cache vieja al entrar. Aunque no se suba, la pagina se pide
    siempre a la red, de modo que el peor caso es que sobren unos escudos.
 */
-var VERSION = "portal-2026-09-06a";
+var VERSION = "portal-2026-09-07a";
 var ESTATICOS = [
  "./",
  "index.html",
